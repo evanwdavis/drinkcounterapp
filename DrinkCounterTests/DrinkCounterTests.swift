5eafccfc3db0ad3d5418a35f647ff7b9ec737e82
Evan Davis <evan.wingfield@me.com>
2016-01-10 23:50:01 -0800

    Initial Commit

---
+++ DrinkCounterTests/DrinkCounterTests.swift
@@ -0,0 +1,36 @@
+//
+//  DrinkCounterTests.swift
+//  DrinkCounterTests
+//
+//  Created by Evan Davis on 1/10/16.
+//  Copyright © 2016 needsabettername. All rights reserved.
+//
+
+import XCTest
+@testable import DrinkCounter
+
+class DrinkCounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
